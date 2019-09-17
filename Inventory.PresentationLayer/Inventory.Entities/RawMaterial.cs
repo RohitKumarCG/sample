0c229b1b6147514910df06cc3cbe02978d439f0f
RohitKumarCG <54537801+RohitKumarCG@users.noreply.github.com>
2019-09-17 11:45:19 +0530

    Update RawMaterial.cs

--- Inventory.PresentationLayer/Inventory.Entities/RawMaterial.cs
+++ Inventory.PresentationLayer/Inventory.Entities/RawMaterial.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,7 @@ namespace Inventory.Entities
     }
 
     //Class for RawMaterial
+
     public class RawMaterial : IRawMaterial
     {
         //auto-implemented properties
